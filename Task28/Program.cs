dfc0f34b59f50799a5fab9c76c0789b871012c14
Валентина <abc99-33@yandex.ru>
2022-08-15 22:44:39 +0300

    Семинар3

---
+++ Task28/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.WriteLine("Введите целое, положительное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int MultiplNumber(int number)
+{
+    int multipl = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        multipl *= i;
+    }
+    return multipl;
+}
+
+int result = MultiplNumber(number);
+Console.WriteLine($"Произведение чисел от 1 до {number} = {result}");
---
+++ Task30/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 30: Напишите программу, которая
+// выводит массив из 8 элементов, заполненный
+// нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+
+void MasRandom(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+}
+void ShowArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    { 
+        if (i < array.Length-1) Console.Write(array[i] + ",");
+        else Console.Write(array[i]);
+    }
+    Console.Write("]");
+}
+Console.WriteLine("");
+int[] mas = new int[8];
+MasRandom(mas);
+ShowArray(mas);
