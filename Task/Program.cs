7d396859748a3d7cf6766de0c4ce60ed0b03cf6b
Валентина <abc99-33@yandex.ru>
2022-08-15 19:23:26 +0300

    Две задачи с семинара

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход координаты точки (Х и Y), 
+// причем Х и Y не равно нулю, и выдает номер четверти плоскости
+
+Console.WriteLine("Введите координаты точки: ");
+Console.Write("Х: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0)
+  Console.WriteLine("Первая четверть");
+ else if (x > 0 && y < 0)
+   Console.WriteLine("Вторая четверть");
+ else if (x < 0 && y < 0)
+   Console.WriteLine("Третья четверть");
+ else if (x < 0 && y > 0)
+   Console.WriteLine("Четвёртая четверть");
+else Console.WriteLine("Введены некорректные координаты");
+
+
---
+++ Task00/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите координаты точки: ");
+Console.Write("X:");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y:");
+int y = Convert.ToInt32(Console.ReadLine());
+
+
+string GetQuarter(int xc, int yc)
+{
+if (xc > 0 && yc > 0) return "Первая четверть";
+if (xc < 0 && yc > 0) return "Вторая четверть";
+if (xc < 0 && yc < 0) return "Третья четверть";
+if (xc > 0 && yc < 0) return "Четвертая четверть";
+
+
+return "Данные введены не корректно";
+}
+string result = GetQuarter(x, y);
+Console.WriteLine(result);
