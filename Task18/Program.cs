f82a24e67524bf86ca516c806a8a62e2087fc273
Валентина <abc99-33@yandex.ru>
2022-08-15 20:25:43 +0300

    Task19

---
+++ Task18/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 18. Напишите программу, которая по заданному 
+// номеру четверти показывает диапозон возможных координат 
+// точек в этой четверти ( X и Y)
+
+Console.WriteLine("Введите номер четверти: ");
+
+int quarter = Convert.ToInt32(Console.ReadLine());
+
+string GetQuarter (int a)
+{
+if (a == 1) return "X > 0, Y > 0";
+if (a == 2) return "X < 0, Y > 0";
+if (a == 3) return "X < 0, Y < 0";
+if (a == 4) return "X > 0, Y < 0";
+return "Данные введены не корректно";
+}
+string result = GetQuarter(quarter);
+Console.WriteLine(result);
---
+++ Task20/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 20:
+// Напишите программу, которая принимает на вход координаты двух точек
+//  и находит расстояние между ними в 2D пространстве
+// А(3,6); В(2,1) -> 5,09
+// А(7,-5); В(1,-1) -> 7,21
+
+// РАССТОЯНИЕ МЕЖДУ ТОЧКАМИ
+// Расстояние между точками А1(х1, y1) и А2(х2, y2) на плоскости с заданными координатами выражается формулой:
+
+Console.WriteLine("Введите координату Х для первой точки: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Y для первой точки: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Х для второй точки: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Y для второй точки: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+Console.WriteLine(Math.Round(result,2));
