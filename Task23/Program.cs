bc8176eabf0f1fc4b98bccb856571309a973e910
Валентина <abc99-33@yandex.ru>
2022-08-16 21:14:28 +0300

    Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите целое, положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int MultiplNumber(int number)
+{
+    int multipl = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        multipl = i*i*i;
+    }
+    return multipl;
+}
+
+int result = MultiplNumber(number);
+Console.WriteLine($"Произведение чисел от 1 до {number} = {result}");
