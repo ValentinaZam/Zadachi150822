a23f34638fec8b8c51b883cb37e829396f11391d
Валентина <abc99-33@yandex.ru>
2022-08-17 22:56:00 +0300

    Task000

--- Task24/Task24Metod/Program.cs
+++ Task24/Task24Metod/Program.cs
@@ -5,14 +5,6 @@
 // 4 -> 10
 // 8 -> 36
 
-
-// int sum = 0;   
-// for (int i = 1; i <= num; i++)
-// {
-//     sum = sum + i;  // sum += i
-// }
-// Console.WriteLine($"Сумма чисел от 1 до {num} = {sum}");
-
 Console.WriteLine("Введите целое, положительное число");
 int num = Convert.ToInt32(Console.ReadLine());
 int SumNumbers(int number)
@@ -24,10 +16,10 @@ int SumNumbers(int number)
     }
     return sum;
 }
-
 if (num > 0)
 {
 int result = SumNumbers(num);
 Console.WriteLine($"Сумма чисел от 1 до {num} = {result}");
 }
-else Console.WriteLine($"Некорректный ввод. Требуется положительное число");
+else 
+Console.WriteLine($"Некорректный ввод. Требуется положительное число");
