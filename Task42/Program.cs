d42793e06f872ded80b67aec52ffa8b487b75808
Валентина <abc99-33@yandex.ru>
2022-08-22 23:20:02 +0300

    Task34

---
+++ Task42/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+
+// C помощью рекурсии
+// int number = 13;
+
+// void DecToBin(int num)
+// {
+//     if(num == 0) return;
+//     DecToBin(num / 2);
+//     Console.Write(num % 2); 
+// }
+
+// DecToBin(number);
+
+Console.WriteLine("Введите целое положительное число: ");
+int num = int.Parse(Console.ReadLine());
+
+int result = 0;
+int mult = 1;
+while(num > 0)
+{
+    result += num % 2 * mult;
+    num = num / 2;
+    mult *= 10;
+}
+
+Console.WriteLine(result);
---
+++ Task44/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 44: Не используя рекурсию, выведите первые N чисел
+// Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+// Через массив:
+Console.Write("Введите число:");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] arr=new int[size];
+arr[0]=0;
+arr[1]=1;
+for (int i = 2; i < size; i++)
+{
+    arr[i]=arr[i-1]+arr[i-2];
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}");
+        if (i != array.Length - 1) Console.Write(",");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+PrintArray(arr);
+
+
+// Не через массив:
+// Console.Write("Введите целое положительное число: ");
+// int number = int.Parse(Console.ReadLine());
+
+// void ShowFibonacci(int num)
+// {
+//     int num1 = 0;
+//     int num2 = 1;
+    
+//     Console.Write(num1+" "+num2+" ");
+
+//     for (int i = 2; i < num; i++)
+//     {
+//         int temp = num1 + num2;
+//         num1 = num2;
+//         num2 = temp;
+//         Console.Write(temp+" ");    
+//     }
+//     Console.WriteLine();
+// }
+
+// ShowFibonacci(number);
